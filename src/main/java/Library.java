893506bf7e37a171b885b5d2a6f2f66fb5550a03
AbramBeazer <32072243+AbramBeazer@users.noreply.github.com>
2021-05-17 12:57:39 -0600

    Library can list books and add book

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,20 @@
-public class Library {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+public class Library {
+    private final List<Book> masterList = new ArrayList<>();
+
+
+    public void addBook(Book book) {
+        masterList.add(book);
+        masterList.sort((book1, book2) -> String.CASE_INSENSITIVE_ORDER.compare(book1.getAuthorLastName(), book2.getAuthorLastName()));
+    }
+
+    public String listAllBooks() {
+        StringBuilder list = new StringBuilder();
+        masterList.forEach(book -> list.append("\"").append(book.getTitle()).append("\" by ").append(book.getAuthorFirstName()).append(book.getAuthorLastName()));
+        return list.toString();
+    }
 }
